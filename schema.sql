3079ce8a2c213fdd36383b941421e911474202d7
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-08-08 21:46:50 +0100

    Added the template for schema

--- schema.sql
+++ schema.sql
@@ -3,3 +3,17 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+
+-- This is how it is on my terminal
+postgres=# CREATE TABLE vet_clinic(
+postgres(# id integer,
+postgres(# date_of_birth date,
+postgres(# escape_attempts integer,
+postgres(# neutered boolean,
+postgres(# weight_kg decimal);
+
+-- I forgot to add the name and i altered it.
+postgres=# ALTER TABLE vet_clinic
+postgres-# ADD name varchar(100);
+ALTER TABLE
+postgres=# \d vet_clinic
